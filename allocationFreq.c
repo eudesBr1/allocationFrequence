5370ce60e149c106128db1dce4b3d65480ef2119
AntoineLal <antoine.nl.lallement@gmail.com>
2023-09-14 12:06:47 +0200

    bibli

--- allocationFreq.c
+++ allocationFreq.c
@@ -3,3 +3,4 @@
 //
 
 #include "allocationFreq.h"
+
--- allocationFreq.h
+++ allocationFreq.h
@@ -2,6 +2,11 @@
 // Created by antoi on 14/09/2023.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "time.h"
+
 #ifndef ALLOCATIONFREQUENCE_ALLOCATIONFREQ_H
 #define ALLOCATIONFREQUENCE_ALLOCATIONFREQ_H
 
